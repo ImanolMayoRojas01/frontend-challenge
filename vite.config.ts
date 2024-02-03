5a19a97f0bf90884199ea8f3695876166c873ab8
Imanol Mayo <mayorojas010501@gmail.com>
2024-02-03 16:57:55 -0500

    refactor: remove packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
-/// <reference types="vitest" />
-/// <reference types="Vite/client" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
@@ -8,16 +5,6 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   test: {
-  //     globals: true,
-  //     enviroment: 'jsdom'
-  //   }
-  // },
-  // test: {
-  //   // globals: true,
-  //   enviroments: 'jsdom'
-  // },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, './src') }
